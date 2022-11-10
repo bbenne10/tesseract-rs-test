b2f7a1ae74835a7a4f0242aa16a66cdd6ee68ac5
Bryan Bennett <Bryan.Bennett@protonmail.com>
2022-11-10 10:29:05 -0500

    nixfmt flake.nix

--- flake.nix
+++ flake.nix
@@ -3,34 +3,24 @@
     flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
   };
-  outputs = {self, nixpkgs, flake-utils, crane }:
+  outputs = { self, nixpkgs, flake-utils, crane }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-          craneLib = crane.lib.${system};
-          buildInputs = with pkgs; [
-            leptonica
-            libiconv
-            tesseract5
-            pkgconfig
-          ];
-          tess_test_deps = craneLib.buildDepsOnly {
-            inherit buildInputs;
-            src = ./.;
-            # Don't run the tests for all of our dependencies.
-            # change if you want to.
-            doCheck = false;
-          };
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.lib.${system};
+        buildInputs = with pkgs; [ leptonica libiconv tesseract5 pkgconfig ];
+        tess_test_deps = craneLib.buildDepsOnly {
+          inherit buildInputs;
+          src = ./.;
+          # Don't run the tests for all of our dependencies.
+          # change if you want to.
+          doCheck = false;
+        };
       in {
         devShell = pkgs.mkShell ({
           # these are persistent inside the shell
-          buildInputs =
-            with pkgs; [
-              rustc
-              cargo
-              clippy
-              rustfmt
-              rust-analyzer
-          ] ++ buildInputs;
+          buildInputs = with pkgs;
+            [ rustc cargo clippy rustfmt rust-analyzer ] ++ buildInputs;
         });
         packages = {
           tess_test = craneLib.buildPackage {
@@ -41,6 +31,5 @@
             doCheck = true;
           };
         };
-      }
-    );
+      });
 }
